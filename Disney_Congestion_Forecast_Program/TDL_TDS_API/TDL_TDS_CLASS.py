4d63c0405c86789923dce4997cda694af22bd5ef
自宅PC <sai11121209@gmail.com>
2020-01-01 02:39:43 +0900

    コメント修正

--- Disney_Congestion_Forecast_Program/TDL_TDS_API/TDL_TDS_CLASS.py
+++ Disney_Congestion_Forecast_Program/TDL_TDS_API/TDL_TDS_CLASS.py
@@ -14,85 +14,85 @@ class facilities:
         self.set_link_url(link_url)
         self.set_upd_original(upd_original)
 
-    #施設ID
+    #ID
     def set_id(self,id):
         self.id = id
     def get_id(self):
         return self.id
 
-    #パーク名
+    #Park_Name
     def set_park(self,park):
         self.park = park
     def get_park(self):
         return  self.park
 
-    #エリアID
+    #Area_ID
     def set_area_id(self,area_id):
         self.area_id = area_id
     def get_area_id(self):
         return  self.area_id
 
-    #エリア名
+    #Area_Name
     def set_area_name(self,area_name):
         self.area_name = area_name
     def get_area_name(self):
         return  self.area_name
 
-    #名前
+    #Name
     def set_name(self,name):
         self.name = name
     def get_name(self):
         return  self.name
 
-    #スタンバイ待ち時間
+    #Standby_Time
     def set_standby_time(self,standby_time):
         self.standby_time = standby_time
     def get_standby_time(self):
         return  self.standby_time
 
-    #営業時間
+    #Operating_Status
     def set_operating_status(self,operating_status):
         self.operating_status = operating_status 
     def get_operating_status(self):
         return  self.operating_status
 
-    #営業状態
+    #Operating_Status_Cd
     def set_operating_status_cd(self,operating_status_cd):
         self.operating_status_cd = operating_status_cd
     def get_operating_status_cd(self):
         return  self.operating_status_cd
 
-    #ファストパス(API上に問題があるため使用禁止)
+    #FastPass
     def set_fp_status(self,fp_status):
         self.fp_status = fp_status
     def get_fp_status(self):
         return  self.fp_status
 
-    #施設座標(緯度)
+    #Facility_Lat
     def set_lat(self,lat):
         self.lat = lat
     def get_lat(self):
         return  self.lat
 
-    #施設座標(経度)
+    #Facility_Lng
     def set_lng(self,lng):
         self.lng = lng
     def get_lng(self):
         return  self.lng
 
-    #施設画像
+    #Image_Url
     def set_image_url(self,image_url):
         self.image_url = image_url
     def get_image_url(self):
         return  self.image_url
 
-    #施設サイト
+    #Link_Url
     def set_link_url(self,link_url):
         self.link_url = link_url
     def get_link_url(self):
         return  self.link_url
 
-    #更新時刻
+    #Upd_Original
     def set_upd_original(self,upd_original):
         self.upd_original = upd_original
     def get_upd_original(self):
