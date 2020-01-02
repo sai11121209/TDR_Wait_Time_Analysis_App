44edd16314d5a438b9168b631b74ce4f89027a4d
saichann <55883274+sai11121209@users.noreply.github.com>
2020-01-02 23:27:49 +0900

    Update SQL_CONNECTER.py

--- Disney_Congestion_Forecast_Program/TDL_TDS_API/SQL_CONNECTER.py
+++ Disney_Congestion_Forecast_Program/TDL_TDS_API/SQL_CONNECTER.py
@@ -1,5 +1,4 @@
 import pyodbc
-from Job_Class import Job_info
 #https://docs.microsoft.com/ja-jp/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-2017
 #SQLSever問い合わせ
 class SQL_Connection:
