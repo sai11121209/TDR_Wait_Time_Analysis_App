f4173aa517b6d19b4831c355c8100a9417911654
自宅PC <sai11121209@gmail.com>
2020-01-05 14:36:26 +0900

    時間同期更新実装

---
+++ Disney_Congestion_Forecast_Program/TDL_TDS_API/Get_Time.py
@@ -0,0 +1,11 @@
+import time
+def Get_Time(Type):     #Type 0:str 1:timedata
+    now = time.ctime()
+    cnvtime = time.strptime(now)
+    if Type is False:
+        return(time.strftime("%Y-%m-%d %H:%M:%S", cnvtime))
+    else:
+        return(cnvtime)
+        
+if __name__ == "__main__":
+    pass
--- Disney_Congestion_Forecast_Program/TDL_TDS_API/SQL_Connector.py
+++ Disney_Congestion_Forecast_Program/TDL_TDS_API/SQL_Connector.py
@@ -1,46 +1,56 @@
 #MySQL/SSL_Connector
-import mysql.connector
+import mysql.connector,os,time
 from sshtunnel import SSHTunnelForwarder
 from Download_Data import Download_Data
-import time,os
+from Get_Time import Get_Time
+
+waittime = int(input('データ取得間隔指定(分):'))
 
 while(1):
-    with SSHTunnelForwarder(
-        ("saichann.shop", 22),
-        ssh_username="saichann",
-        ssh_password="1o1gxdlSYkes",
-        remote_bind_address=("127.0.0.1",3306)
-    ) as server:
-        server.start()
-        print('SSL:ok')
-        connect_args = {
-            "host": "localhost",
-            "port": server.local_bind_port,
-            "user": "saichann",
-            "password": "Yuta1209",
-            "database" : 'saichann',
-        }
-        
-        db = mysql.connector.connect(**connect_args)
-        print('MySQL:'+str(db.is_connected()))
-        db.ping(reconnect=True)
-        cursor = db.cursor(named_tuple=True)
-        datas = Download_Data()
-        #for data in datas:
-            #sql = 'INSERT INTO Facility (Facility_ID, Facility_Name, Facility_URL) VALUE ('+data.get_id()+',"'+data.get_name()+'","'+data.get_link_url()+'");'
-            #print(sql)
-            #cursor.execute(sql)
-            #db.commit()
-        for data in datas:
-            if len(data.get_standby_time()) == 0:
-                sql = 'INSERT INTO Standby_Time (Facility_ID) VALUE ('+data.get_id()+');'
-            else:
-                sql = 'INSERT INTO Standby_Time (Facility_ID,Standby_Time) VALUE ('+data.get_id()+',"'+data.get_standby_time()+'");'
-            print(sql)
-            cursor.execute(sql)
-        db.commit()
-        cursor.close()
-        db.close()
-        server.stop()
-    time.sleep(300)
-    print('-------------------------')
+    Timedata = Get_Time(1)
+    Time_H = int(time.strftime("%H",Timedata))
+    Time_M = int(time.strftime("%M",Timedata))
+    Time_S = int(time.strftime("%S",Timedata))
+    if Time_M % waittime == 0 and Time_S == 57 :
+        with SSHTunnelForwarder(
+            ("saichann.shop", 22),
+            ssh_username="saichann",
+            ssh_password="1o1gxdlSYkes",
+            remote_bind_address=("127.0.0.1",3306)
+        ) as server:
+            server.start()
+            print('SSL:ok')
+            connect_args = {
+                "host": "localhost",
+                "port": server.local_bind_port,
+                "user": "saichann",
+                "password": "Yuta1209",
+                "database" : 'saichann',
+            }
+            
+            db = mysql.connector.connect(**connect_args)
+            print('MySQL:'+str(db.is_connected()))
+            db.ping(reconnect=True)
+            cursor = db.cursor(named_tuple=True)
+            datas = Download_Data()
+            #for data in datas:
+                #sql = 'INSERT INTO Facility (Facility_ID, Facility_Name, Facility_URL) VALUE ('+data.get_id()+',"'+data.get_name()+'","'+data.get_link_url()+'");'
+                #print(sql)
+                #cursor.execute(sql)
+                #db.commit()
+            for data in datas:
+                if len(data.get_standby_time()) == 0:
+                    sql = 'INSERT INTO Standby_Time (Facility_ID) VALUE ('+data.get_id()+');'
+                else:
+                    sql = 'INSERT INTO Standby_Time (Facility_ID,Standby_Time) VALUE ('+data.get_id()+',"'+data.get_standby_time()+'");'
+                print(sql,end='')
+                cursor.execute(sql)
+                print('ok')
+                print('')
+            db.commit()
+            cursor.close()
+            db.close()
+            server.stop()
+        print(f"The next data acquisition time is {Time_H}:{Time_M+waittime}:{Time_S}")
+        time.sleep(waittime*60)
+    #sql = 'ALTER TABLE `Standby_Time` auto_increment = 1'
