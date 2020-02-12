2fd96646f6aff00264df2cb841c0480363b3df49
saichann <55883274+sai11121209@users.noreply.github.com>
2020-02-12 18:43:14 +0900

    Create SQL_Connector_PLT.py

---
+++ Disney_Congestion_Forecast_Program/TDL_TDS_API_Client/SQL_Connector_PLT.py
@@ -0,0 +1,46 @@
+import mysql.connector,copy,datetime,Query
+from sshtunnel import SSHTunnelForwarder
+
+def Get_Data(ID):
+    with SSHTunnelForwarder(
+        ("saichann.shop", 22),
+        ssh_username="saichann",
+        ssh_password="1o1gxdlSYkes",
+        remote_bind_address=("127.0.0.1", 3306)
+    ) as server:
+        server.start()
+        print('SSL:ok')
+        connect_args = {
+            "host": "localhost",
+            "port": server.local_bind_port,
+            "user": "saichann",
+            "password": "Yuta1209",
+            "database" : 'saichann',
+        }
+        
+        db = mysql.connector.connect(**connect_args)
+        print('MySQL:'+str(db.is_connected()))
+        db.ping(reconnect=True)
+        cursor = db.cursor(named_tuple=False)
+        cursor.execute(Query.Sel_All_Wait_Time(ID))
+        rows = cursor.fetchall()
+        waittime,date = [],[]
+        n = 0
+        for row in rows:
+            if row[0] != None:
+                waittime.append(int(row[0]))
+            else:
+                waittime.append(1)
+            date.append(row[1])
+            n += 1
+        cursor.execute(Query.Sel_All_Wait_Time(ID))
+        rows = cursor.fetchall()
+        cursor.close()
+        print('MySQL:Disconnected')
+        db.close()
+        server.stop()
+        print('SSL:Close')
+    return waittime,date
+
+if __name__ == '__main__':
+    pass
